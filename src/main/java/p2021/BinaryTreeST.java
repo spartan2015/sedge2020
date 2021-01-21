438671533f3cc26164d240635932a05ee46eed7e
spartan2015 <vasile.irimia@aurea.com>
2021-01-21 15:19:26 +0200

    next

--- src/main/java/p2021/BinaryTreeST.java
+++ src/main/java/p2021/BinaryTreeST.java
@@ -9,7 +9,153 @@ import static org.junit.Assert.assertEquals;
  */
 public class BinaryTreeST<Key extends Comparable> {
 
-    static class Node<Key extends Comparable>{
+    Node<Key> root;
+
+    public void insert(Key key, Object value) {
+        root = insert(root, key, value);
+    }
+
+    public Key floor(Key given) {
+        Node<Key> floor = floor(root, given);
+        if (floor == null) return null;
+        return floor.key;
+    }
+
+    public Node<Key> floor(Node<Key> start, Key given) {
+        for (Node<Key> current = start; current != null; ) {
+            int cmp = given.compareTo(current);
+            if (cmp == 0) {
+                return current;
+            } else if (cmp < 0) {
+                current = current.left;
+            } else { // first < given - candidate
+                Node tryRight = floor(current.right, given);
+                return tryRight != null ? tryRight : current;
+            }
+        }
+        return null;
+    }
+
+    public Key ceiling(Key given) {
+        Node<Key> ceiling = ceiling(root, given);
+        return ceiling != null ? ceiling.key : null;
+    }
+
+    @Test
+    public void t1() {
+        BinaryTreeST<Integer> st = new BinaryTreeST<>();
+
+        st.insert(10, 10);
+
+        assertEquals(Integer.valueOf(10), st.select(1).key);
+
+        st.insert(6, 6);
+        assertEquals(Integer.valueOf(6), st.select(1).key);
+
+        st.insert(2, 2);
+        assertEquals(Integer.valueOf(2), st.select(1).key);
+
+        st.insert(16, 16);
+        assertEquals(Integer.valueOf(16), st.select(4).key);
+        assertEquals(Integer.valueOf(10), st.select(3).key);
+        assertEquals(Integer.valueOf(6), st.select(2).key);
+        assertEquals(Integer.valueOf(2), st.select(1).key);
+    }
+
+    Node<Key> ceiling(Node<Key> start, Key given) {
+        for (Node<Key> current = start; current != null; ) {
+            int cmp = given.compareTo(current);
+            if (cmp == 0) {
+                return current;
+            } else if (cmp > 0) {
+                current = current.right;
+            } else { // current is greater - is a possibily
+                Node<Key> tryLeft = ceiling(current.left, given);
+                return tryLeft != null ? tryLeft : current;
+            }
+        }
+        return null;
+    }
+
+    Node<Key> select(int k) {
+        return select(root, k, 0);
+    }
+
+    Node<Key> select(Node<Key> start, int k, int add) {
+        for (Node<Key> current = start; current != null; ) {
+            int currentK = current.size - size(current.right) + add;
+            if (currentK == k) {
+                return current;
+            } else if (currentK > k) {
+                add = 0;
+                current = current.left;
+            } else {
+                add = currentK;
+                current = current.right;
+            }
+        }
+        return null;
+    }
+
+    void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    Node<Key> delete(Node<Key> current, Key key) {
+        if (current == null) return null;
+        int cmp = key.compareTo(current.key);
+        if (cmp == 0) {
+            if (current.left == null) {
+                return current.right;
+            } else {
+                Node<Key> largestParent = current.left;
+                Node<Key> largest = current.left;
+                for (; current.right != null; current = current.right) {
+                    largestParent = largest;
+                    largest = current.right;
+                }
+                largestParent.right = largest.left;
+
+                largestParent.size = largest.left.size + 1;
+                largest.size = current.size - 1;
+
+                largest.right = current.right;
+                largest.left = current.left;
+
+                return largest;
+            }
+        } else if (cmp < 0) {
+            return delete(current.left, key);
+        } else if (cmp > 0) {
+            return delete(current.right, key);
+        }
+        current.size = size(current.left) + 1 + size(current.right);
+        return current;
+    }
+
+    int size(Node<Key> node) {
+        return node != null ? node.size : 0;
+    }
+
+    private Node<Key> insert(Node<Key> node, Key key, Object value) {
+        if (node == null) {
+            return new Node<>(key, value, 1);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = insert(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = insert(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        node.size = size(node.left) + 1 + size(node.right);
+
+        return node;
+    }
+
+    static class Node<Key extends Comparable> {
         Key key;
         Object value;
         int size;
@@ -21,116 +167,6 @@ public class BinaryTreeST<Key extends Comparable> {
             this.value = value;
             this.size = size;
         }
-    }
-
-    Node<Key> root;
-
-    public void insert(Key key, Object value){
-        root = insert(root, key, value);
-    }
-
-    private Node<Key> insert(Node<Key> node, Key key, Object value) {
-        if (node == null){
-            return new Node<>(key, value, 1);
-        }
-
-        int cmp = key.compareTo(node.key);
-        if (cmp < 0){
-            node.left = insert(node.left, key, value);
-        }else if (cmp > 0){
-            node.right = insert(node.right, key,value);
-        }else{
-            node.value = value;
-        }
-        node.size = size(node.left) + 1 + size(node.right);
-
-        return node;
-    }
-
-    public Key floor(Key given){
-        Node<Key> floor = floor(root, given);
-        if (floor == null) return null;
-        return floor.key;
-    }
-
-    public Node<Key> floor(Node<Key> start, Key given){
-        for(Node<Key> current = start; current!=null;){
-            int cmp = given.compareTo(current);
-            if (cmp == 0){
-                return current;
-            }else if (cmp < 0){
-                current = current.left;
-            } else { // first < given - candidate
-                Node tryRight = floor(current.right, given);
-                return tryRight!=null ? tryRight : current;
-            }
-        }
-        return null;
-    }
-
-    public Key ceiling(Key given){
-        Node<Key> ceiling = ceiling(root, given);
-        return ceiling!=null ? ceiling.key : null;
-    }
-
-    Node<Key> ceiling(Node<Key> start, Key given){
-        for(Node<Key> current = start; current!=null;){
-            int cmp = given.compareTo(current);
-            if (cmp == 0){
-                return current;
-            }else if (cmp > 0){
-                current = current.right;
-            }else { // current is greater - is a possibily
-                Node<Key> tryLeft = ceiling(current.left, given);
-                return tryLeft!=null ? tryLeft : current;
-            }
-        }
-        return null;
-    }
-
-    Node<Key> select(int k) {
-        return select(root, k, 0);
-    }
-
-    Node<Key> select(Node<Key> start, int k, int add){
-        for(Node<Key> current = start; current!=null; ){
-            int currentK = current.size - size(current.right) + add;
-            if (currentK == k){
-                return current;
-            }else if (currentK > k){
-                add = 0;
-                current = current.left;
-            }else{
-                add = currentK;
-                current = current.right;
-            }
-        }
-        return null;
-    }
-
-    int size(Node<Key> node){
-        return node!=null ? node.size: 0;
-    }
-
-    @Test
-    public void t1(){
-        BinaryTreeST<Integer> st = new BinaryTreeST<>();
-
-        st.insert(10,10);;
-
-        assertEquals(Integer.valueOf(10), st.select(1).key);
-
-        st.insert(6,6);
-        assertEquals(Integer.valueOf(6), st.select(1).key);
-
-        st.insert(2,2);
-        assertEquals(Integer.valueOf(2), st.select(1).key);
-
-        st.insert(16,16);
-        assertEquals(Integer.valueOf(16), st.select(4).key);
-        assertEquals(Integer.valueOf(10), st.select(3).key);
-        assertEquals(Integer.valueOf(6), st.select(2).key);
-        assertEquals(Integer.valueOf(2), st.select(1).key);
     }
 
 }
---
+++ src/main/java/p2021/RedBlackBST.java
@@ -0,0 +1,195 @@
+package p2021;
+
+/**
+ * RedBlackBST
+ */
+public class RedBlackBST<Key extends Comparable, Value> {
+
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private Node root;
+
+    public void deleteMin() {
+        if (!isRed(root.left) && !isRed(root.right)) root.color = RED;
+        root = deleteMin(root);
+        if (root != null) root.color = BLACK;
+    }
+
+    public void deleteMax() {
+        if (!isRed(root.left) && !isRed(root.right)) root.color = RED;
+        root = deleteMax(root);
+        if (root != null) root.color = BLACK;
+    }
+
+    public void delete(Key key) {
+        if (!isRed(root.left) && !isRed(root.right)) root.color = RED;
+        root = delete(root, key);
+        if (!isEmpty()) root.color = BLACK;
+    }
+
+    public void insert(Key key, Value value) {
+        root = insert(root, key, value);
+        root.color = BLACK;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+
+        x.color = h.color;
+        h.color = RED;
+
+        x.size = h.size;
+        h.size = 1 + size(h.left) + size(h.right);
+
+        return x;
+    }
+
+    Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+
+        x.color = h.color;
+        h.color = RED;
+
+        x.size = h.size;
+        h.size = 1 + size(h.left) + size(h.right);
+
+        return x;
+    }
+
+    int size(Node x) {
+        return x == null ? 0 : x.size;
+    }
+
+    Node insert(Node h, Key key, Value value) {
+        if (h == null) return new Node(key, value, null, null, 1, RED);
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) {
+            h.left = insert(h.left, key, value);
+        } else if (cmp > 0) {
+            h.right = insert(h.right, key, value);
+        } else {
+            h.value = value;
+        }
+
+        h = balance(h);
+
+        h.size = 1 + size(h.left) + size(h.right);
+        return h;
+    }
+
+    void flipColors(Node h) {
+        h.left.color = !h.left.color;
+        h.right.color = !h.right.color;
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color = RED;
+    }
+
+    private Node balance(Node h) {
+        if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+        return h;
+    }
+
+    private Node moveRedLeft(Node h) { // Assuming that h is red and both h.left and h.left.left
+// are black, make h.left or one of its children red.
+        flipColors(h);
+        if (isRed(h.right.left)) {
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+        }
+        return h;
+    }
+
+    private Node deleteMin(Node h) {
+        if (h.left == null) return null;
+
+        if (!isRed(h.left) && !isRed(h.left.left)) h = moveRedLeft(h);
+
+        h.left = deleteMin(h.left);
+
+        return balance(h);
+    }
+
+    private Node moveRedRight(Node h) { // Assuming that h is red and both h.right and h.right.left
+// are black, make h.right or one of its children red.
+        flipColors(h);
+        if (!isRed(h.left.left)) h = rotateRight(h);
+        return h;
+    }
+
+    private Node deleteMax(Node h) {
+        if (isRed(h.left)) h = rotateRight(h);
+
+        if (h.right == null) return null;
+
+        if (!isRed(h.right) && !isRed(h.right.left)) h = moveRedRight(h);
+
+        h.right = deleteMax(h.right);
+
+        return balance(h);
+    }
+
+    private Node delete(Node h, Key key) {
+        if (key.compareTo(h.key) < 0) {
+            if (!isRed(h.left) && !isRed(h.left.left)) h = moveRedLeft(h);
+            h.left = delete(h.left, key);
+        } else {
+            if (isRed(h.left)) h = rotateRight(h);
+            if (key.compareTo(h.key) == 0 && (h.right == null)) return null;
+            if (!isRed(h.right) && !isRed(h.right.left)) h = moveRedRight(h);
+            if (key.compareTo(h.key) == 0) {
+                h.value = get(h.right, min(h.right).key);
+                h.key = min(h.right).key;
+                h.right = deleteMin(h.right);
+            } else h.right = delete(h.right, key);
+        }
+        return balance(h);
+    }
+
+    private Value get(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x.value;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) return x;
+        return min(x.left);
+    }
+
+    private class Node {
+        Key key;
+        Value value;
+        Node left, right;
+        int size;
+        boolean color;
+
+        public Node(Key key, Value value, Node left, Node right, int size, boolean color) {
+            this.key = key;
+            this.value = value;
+            this.left = left;
+            this.right = right;
+            this.size = size;
+            this.color = color;
+        }
+    }
+}
